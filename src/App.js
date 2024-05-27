2b43b092a668704eefb14544fb19a62c7054a06a
Ivonne <ivonnebenites2015@gmail.com>
2024-05-27 07:40:13 -0500

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./components/Weather";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Lima" />
 
-export default App;
+        <footer>
+          This project was code by {""}
+          <a
+            href="https://ivonnebenitesrodriguez.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ivonne Benites
+          </a>
+          {""}
+          and is {""}
+          <a
+            href="https://github.com/IvonneBenitesRodriguez/weather_app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+          {""}
+          and {""}
+          <a href="" target="_blank" rel="noopener noreferrer">
+            hosted on Vercel
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
